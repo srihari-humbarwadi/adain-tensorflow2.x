205f46c3384b1163ecbf62c0ef5f9a1cda5e2e1d
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-12-24 15:32:39 +0530

    Implemented ReflectionPadding2D, tf.math.divide_no_nan for safe division

--- adain/model/layers/adaptive_instance_normalization.py
+++ adain/model/layers/adaptive_instance_normalization.py
@@ -16,9 +16,25 @@ class AdaptiveInstanceNormalization(tf.keras.layers.Layer):
         style_std = tf.sqrt(style_variance + tf.keras.backend.epsilon())
         content_std = tf.sqrt(content_variance + tf.keras.backend.epsilon())
 
-        normalized_content_images = (
-            content_images - content_mean) / content_std
+        normalized_content_images = tf.math.divide_no_nan(
+            content_images - content_mean, content_std)
         return style_std * normalized_content_images + style_mean
 
     def get_config(self):
         return super(AdaptiveInstanceNormalization, self).get_config()
+
+
+class ReflectionPadding2D(tf.keras.layers.Layer):
+
+    def __init__(self, padding, **kwargs):
+        super(ReflectionPadding2D, self).__init__(**kwargs)
+
+        self.paddings = ((0, 0), (padding, padding), (padding, padding), (0, 0))
+
+    def call(self, x):
+        return tf.pad(x, paddings=self.paddings, mode='REFLECT')
+
+    def get_config(self):
+        config = {'paddings': self.paddings}
+        base_config = super(ReflectionPadding2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
