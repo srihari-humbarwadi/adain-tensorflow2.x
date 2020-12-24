2dece6a139c506949c3535b85166571aa7c0f132
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-12-24 15:37:26 +0530

    Added support to set epsilon in AdaptiveInstanceNormalization

--- adain/model/layers/adaptive_instance_normalization.py
+++ adain/model/layers/adaptive_instance_normalization.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 class AdaptiveInstanceNormalization(tf.keras.layers.Layer):
 
     def __init__(self, **kwargs):
+        self.epsilon = kwargs.pop('epsilon', tf.keras.backend.epsilon())
         super(AdaptiveInstanceNormalization, self).__init__(**kwargs)
 
     def call(self, inputs):
@@ -13,15 +14,17 @@ class AdaptiveInstanceNormalization(tf.keras.layers.Layer):
             tf.nn.moments(style_images, axes=[1, 2], keepdims=True)
         content_mean, content_variance = \
             tf.nn.moments(content_images, axes=[1, 2], keepdims=True)
-        style_std = tf.sqrt(style_variance + tf.keras.backend.epsilon())
-        content_std = tf.sqrt(content_variance + tf.keras.backend.epsilon())
+        style_std = tf.sqrt(style_variance + self.epsilon)
+        content_std = tf.sqrt(content_variance + self.epsilon)
 
         normalized_content_images = tf.math.divide_no_nan(
             content_images - content_mean, content_std)
         return style_std * normalized_content_images + style_mean
 
     def get_config(self):
-        return super(AdaptiveInstanceNormalization, self).get_config()
+        config = {'epsilon': self.epsilon}
+        base_config = super(AdaptiveInstanceNormalization, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
 class ReflectionPadding2D(tf.keras.layers.Layer):
