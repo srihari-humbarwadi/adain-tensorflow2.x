dcba4ae195f9ca69811d9a7fdae261bc1d7a47fc
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-12-24 13:50:32 +0530

    Implemented AdaptiveInstanceNormalization

---
+++ adain/model/layers/__init__.py
@@ -0,0 +1,3 @@
+from adain.model.layers.adaptive_instance_normalization import AdaptiveInstanceNormalization  # noqa: E501
+
+__all__ = ['AdaptiveInstanceNormalization']
---
+++ adain/model/layers/adaptive_instance_normalization.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+
+class AdaptiveInstanceNormalization(tf.keras.layers.Layer):
+
+    def __init__(self, **kwargs):
+        super(AdaptiveInstanceNormalization, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        style_images, content_images = inputs
+
+        style_mean, style_variance = \
+            tf.nn.moments(style_images, axes=[1, 2], keepdims=True)
+        content_mean, content_variance = \
+            tf.nn.moments(content_images, axes=[1, 2], keepdims=True)
+        style_std = tf.sqrt(style_variance + tf.keras.backend.epsilon())
+        content_std = tf.sqrt(content_variance + tf.keras.backend.epsilon())
+
+        normalized_content_images = (
+            content_images - content_mean) / content_std
+        return style_std * normalized_content_images + style_mean
+
+    def get_config(self):
+        return super(AdaptiveInstanceNormalization, self).get_config()
