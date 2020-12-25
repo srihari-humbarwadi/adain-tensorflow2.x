f20abf0c39b5c173d3949dbf5db143c0dd6ab239
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-12-25 15:56:44 +0530

    Added content-style tradeoff controll in adaptive_instance_normalization

--- adain/model/layers/adaptive_instance_normalization.py
+++ adain/model/layers/adaptive_instance_normalization.py
@@ -3,25 +3,27 @@ import tensorflow as tf
 
 class AdaptiveInstanceNormalization(tf.keras.layers.Layer):
 
-    def __init__(self, **kwargs):
+    def __init__(self, alpha=1.0, **kwargs):
         self.epsilon = kwargs.pop('epsilon', tf.keras.backend.epsilon())
         super(AdaptiveInstanceNormalization, self).__init__(**kwargs)
+        self.alpha = alpha
 
     def call(self, inputs):
-        style_images, content_images = inputs
+        style_features, content_features = inputs
 
         style_mean, style_variance = \
-            tf.nn.moments(style_images, axes=[1, 2], keepdims=True)
+            tf.nn.moments(style_features, axes=[1, 2], keepdims=True)
         content_mean, content_variance = \
-            tf.nn.moments(content_images, axes=[1, 2], keepdims=True)
+            tf.nn.moments(content_features, axes=[1, 2], keepdims=True)
         style_std = tf.sqrt(style_variance + self.epsilon)
         content_std = tf.sqrt(content_variance + self.epsilon)
 
-        normalized_content_images = tf.math.divide_no_nan(
-            content_images - content_mean, content_std)
-        return style_std * normalized_content_images + style_mean
+        normalized_content_features = tf.math.divide_no_nan(
+            content_features - content_mean, content_std)
+        output = style_std * normalized_content_features + style_mean
+        return self.alpha * output + (1 - self.alpha) * content_features
 
     def get_config(self):
-        config = {'epsilon': self.epsilon}
+        config = {'alpha': self.alpha, 'epsilon': self.epsilon}
         base_config = super(AdaptiveInstanceNormalization, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
