b5bc73f93958296ab7250a60e70bbdc608346627
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-12-26 08:28:20 +0530

    add distributed training utilts

---
+++ adain/distribute.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from absl import logging
+
+
+def get_strategy(params):
+    if params.type == 'gpu':
+        logging.info('Creating GPU strategy')
+        return tf.distribute.OneDeviceStrategy(device='/gpu:0')
+
+    if params.type == 'cpu':
+        logging.info('Creating CPU strategy')
+        return tf.distribute.OneDeviceStrategy(device='/cpu:0')
+
+    if params.type == 'multi_gpu':
+        logging.info('Creating Multi GPU strategy')
+        return tf.distribute.MirroredStrategy()
+
+    if params.type == 'tpu':
+        logging.info('Creating TPU strategy')
+        resolver = tf.distribute.cluster_resolver.TPUClusterResolver.connect(
+            params.name)
+        return tf.distribute.TPUStrategy(resolver)
+    raise ValueError('Unsupported strategy requested')
