e959c1b1e8d92e424b70c9348a5ce19d3c9dd6a0
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-10-08 00:00:28 +0530

    changed eval image resize method

--- adain/dataloader/preprocessing_pipeline.py
+++ adain/dataloader/preprocessing_pipeline.py
@@ -23,17 +23,10 @@ class PreprocessingPipeline:
 
     def _resize(self, image):
         image_size = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
-        max_side = tf.maximum(image_size[0], image_size[1])
-
-        if max_side > self.augmentation_params.max_side:
-            ratio = self.augmentation_params.max_side / max_side
-            new_image_size = tf.cast(image_size * ratio, dtype=tf.int32)
-            image = tf.image.resize(image, size=new_image_size)
-
-        else:
-            ratio = 1.0
-            new_image_size = tf.cast(image_size, dtype=tf.int32)
-
+        ratio = self.augmentation_params.min_side / tf.minimum(
+            image_size[0], image_size[1])
+        new_image_size = tf.cast(image_size * ratio, dtype=tf.int32)
+        image = tf.image.resize(image, size=new_image_size)
         return image, image_size, new_image_size, ratio
 
     def inference_pipeline(self, images):
