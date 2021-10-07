0a5026fc1ffb34198155258e2cfca0f7beef5acc
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-10-07 21:59:58 +0530

    postprocessing changes

--- adain/dataloader/preprocessing_pipeline.py
+++ adain/dataloader/preprocessing_pipeline.py
@@ -50,22 +50,19 @@ class PreprocessingPipeline:
         return tf.expand_dims(image, axis=0)
 
     def denormalize(self, images):
-        image = images[0]
-        offset = tf.reshape(tf.constant(self.preprocessing_params.offset),
-                            shape=[1, 1, 3])
-        scale = tf.reshape(tf.constant(self.preprocessing_params.scale),
-                           shape=[1, 1, 3])
+        pixel_mins = tf.reduce_min(images, axis=[1, 2, 3])
+        pixel_maxs = tf.reduce_max(images, axis=[1, 2, 3])
+        pixel_range = pixel_maxs - pixel_mins
 
-        image = image * scale + offset
+        images = 255.0 * (images - pixel_mins) / pixel_range
+        images = tf.clip_by_value(images, 0, 255)
 
         if self.preprocessing_params.use_bgr:
-            image = image[:, :, ::-1]
+            images = images[:, :, :, ::-1]
 
-        image = tf.clip_by_value(image, 0, 255)
+        return tf.cast(images, dtype=tf.uint8)
 
-        return tf.cast(tf.expand_dims(image, axis=0), dtype=tf.uint8)
-
-    def __call__(self, sample, return_labels=False):
+    def __call__(self, sample):
         image = sample["image"]
 
         if not self.is_validation_dataset:
