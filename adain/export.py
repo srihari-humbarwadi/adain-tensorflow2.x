00c036d1c168de9dd3a19bb2cb1c5af743910a25
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-10-10 14:10:26 +0530

    freeze graph before exporting `saved_model`

--- adain/export.py
+++ adain/export.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from absl import app, flags, logging
+from tensorflow.python.framework.convert_to_constants import \
+    convert_variables_to_constants_v2_as_graph
 
 from adain.cfg import Config
 from adain.dataloader.preprocessing_pipeline import PreprocessingPipeline
@@ -28,23 +30,24 @@ def main(_):
 
     params = Config(FLAGS.config_path).params
 
-    trainer = Trainer(run_mode='export',  # noqa: F841
-                      strategy=tf.distribute.OneDeviceStrategy(device='/cpu:0'),  # noqa: E501
-                      model_fn=model_builder(params),
-                      train_input_fn=None,
-                      val_input_fn=None,
-                      style_loss_weight=params.training.style_loss_weight,
-                      content_loss_weight=params.training.content_loss_weight,
-                      train_steps=params.training.train_steps,
-                      val_steps=params.training.validation_steps,
-                      val_freq=params.training.validation_freq,
-                      steps_per_execution=params.training.steps_per_execution,
-                      batch_size=params.training.batch_size,
-                      model_dir=params.experiment.model_dir,
-                      save_every=params.training.save_every,
-                      restore_checkpoint=params.training.restore_checkpoint,
-                      summary_dir=params.experiment.tensorboard_dir,
-                      name=params.experiment.name)
+    trainer = Trainer(
+        run_mode='export',  # noqa: F841
+        strategy=tf.distribute.OneDeviceStrategy(device='/cpu:0'),  # noqa: E501
+        model_fn=model_builder(params),
+        train_input_fn=None,
+        val_input_fn=None,
+        style_loss_weight=params.training.style_loss_weight,
+        content_loss_weight=params.training.content_loss_weight,
+        train_steps=params.training.train_steps,
+        val_steps=params.training.validation_steps,
+        val_freq=params.training.validation_freq,
+        steps_per_execution=params.training.steps_per_execution,
+        batch_size=params.training.batch_size,
+        model_dir=params.experiment.model_dir,
+        save_every=params.training.save_every,
+        restore_checkpoint=params.training.restore_checkpoint,
+        summary_dir=params.experiment.tensorboard_dir,
+        name=params.experiment.name)
 
     inference_model = trainer.model
     inference_model.build([(1, None, None, 3), (1, None, None, 3), ()])
@@ -53,15 +56,18 @@ def main(_):
     inference_model.compiled_metrics = None
     inference_model._metrics = []
 
-    input_signature = [(
-        tf.TensorSpec(shape=[1, None, None, 3],
-                      name='style_images',
-                      dtype=tf.float32),
-        tf.TensorSpec(shape=[1, None, None, 3],
-                      name='content_images',
-                      dtype=tf.float32),
-        tf.TensorSpec(shape=[], name='alpha', dtype=tf.float32),
-    )]
+    input_signature = {
+        'style_images': tf.TensorSpec(
+            shape=[1, None, None, 3],
+            name='style_images',
+            dtype=tf.float32),
+        'content_images': tf.TensorSpec(
+            shape=[1, None, None, 3],
+            name='content_images',
+            dtype=tf.float32),
+        'alpha': tf.TensorSpec(
+            shape=[], name='alpha', dtype=tf.float32)
+    }
 
     logging.debug('Signature for serving_default :\n{}'.format(
         input_signature))
@@ -71,27 +77,61 @@ def main(_):
     preprocessing_pipeline = PreprocessingPipeline(params=params,
                                                    is_validation_dataset=True)
 
-    @tf.function(input_signature=input_signature)
-    def serving_fn(input_data):
-        style_images, content_images, alpha = input_data
-
-        style_images = preprocessing_pipeline.inference_pipeline(style_images)
+    @tf.function
+    def serving_fn(sample):
+        style_images = preprocessing_pipeline.inference_pipeline(
+            sample['style_images'])
         content_images = preprocessing_pipeline.inference_pipeline(
-            content_images)
+            sample['content_images'])
 
-        generated_images = inference_model.call(
-            (style_images, content_images, alpha),
+        stylized_images = inference_model.call((
+            style_images,
+            content_images,
+            sample['alpha']),
             training=False)['synthesized_images']
 
-        generated_images = preprocessing_pipeline.denormalize(generated_images)
-        return {'generated_images': generated_images}
+        stylized_images = preprocessing_pipeline.denormalize(stylized_images)
+        return {'stylized_images': stylized_images}
+
+    frozen_serving_fn, _ = convert_variables_to_constants_v2_as_graph(
+        serving_fn.get_concrete_function(input_signature),
+        aggressive_inlining=True)
+
+    class InferenceModule(tf.Module):
+        def __init__(self, inference_function):
+            super(InferenceModule, self).__init__(name='inference_module')
+            self.inference_function = inference_function
+
+        @tf.function
+        def run_inference(self, sample):
+            outputs = self.inference_function(**sample)
+            return {'stylized_images': outputs[0]}
+
+    inference_module = InferenceModule(inference_function=frozen_serving_fn)
+    signatures = {
+        'serving_default': inference_module.run_inference.get_concrete_function(
+            input_signature)
+    }
+    print(signatures['serving_default'].output_shapes)
+    for _signature_name, _concrete_fn in signatures.items():
+        input_shapes = {x.name.split(':')[0]: x.shape.as_list()
+                        for x in _concrete_fn.inputs}
+
+        output_shapes = {k: v.as_list()
+                         for k, v in _concrete_fn.output_shapes.items()}
+        logging.info(
+            '\nSignature: {}\n Input Shapes:\n {}\nOutput Shapes:\n{}'.format(
+                _signature_name,
+                input_shapes,
+                output_shapes))
 
     logging.info('Exporting `saved_model` to {}'.format(FLAGS.export_dir))
-
     tf.saved_model.save(
-        inference_model,
-        FLAGS.export_dir,
-        signatures={'serving_default': serving_fn.get_concrete_function()})  # noqa: E501
+        obj=inference_module,
+        export_dir=FLAGS.export_dir,
+        signatures=signatures,
+        options=tf.saved_model.SaveOptions(
+            experimental_custom_gradients=False))
 
 
 if __name__ == '__main__':
